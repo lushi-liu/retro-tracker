a4e447441c2890b6b70b945ed2299c6823b04a61
Lushi Liu <lushiliu1@gmail.com>
2025-08-31 16:07:19 -0600

    updated route.ts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,7 +4,10 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const { db } = await connectToDatabase();
-    const posts = await db.collection("posts").find().toArray();
+    const posts = await db
+      .collection("posts")
+      .find({ sentiment: { $exists: true } })
+      .toArray();
     return NextResponse.json(posts);
   } catch (error) {
     return NextResponse.json(
